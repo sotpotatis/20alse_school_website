e8dda55f7422aee566711679822073dae263ca1b
sotpotatis <35110380+William04A@users.noreply.github.com>
2022-06-08 21:15:07 +0200

    Updates - first revision is now published

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,12 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.env.NODE_ENV === "development";
 const config = {
 	kit: {
 		adapter: adapter(),
-
+		prerender: {
+			default: true // This is to get a static website as output. Thanks Nyx#3318 at the Svelte Discord server for the solution!
+		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
